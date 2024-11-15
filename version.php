242eb5ac9064ed830f509a7caec3da8fe713934a
MohammadReza PourMohammad <mrpm.root@gmail.com>
2024-11-15 18:12:39 +0330

    Update version

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'enrol_cart';
-$plugin->version = 2024111500;
+$plugin->version = 2024111501;
 $plugin->requires = 2020110300;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.1.0';
